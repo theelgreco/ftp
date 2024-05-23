3fcd2243606cfe2ea350163ac538a3cb984c54f7
theelgreco <stelios.kakoliris@yahoo.com>
2024-05-23 14:56:12 +0100

    added frontend functionality

--- ftp-client/src/main.js
+++ ftp-client/src/main.js
@@ -8,11 +8,12 @@ import router from "@/router/index.js";
 import axios from "axios";
 
 import PrimeVue from 'primevue/config';
+import DialogService from 'primevue/dialogservice';
 
 const baseAxios = axios.create({
-  baseURL: process.env.NODE_ENV === "development" ? "http://127.0.0.1:8080" : "",
+    baseURL: process.env.NODE_ENV === "development" ? "http://127.0.0.1:8080" : "",
 });
 
 const app = createApp(App)
 app.config.globalProperties.$http = baseAxios
-app.use(router).use(PrimeVue).mount('#app')
+app.use(router).use(PrimeVue).use(DialogService).mount('#app')
