8a097482ffee2c25882dae8051c72772238eca2c
theelgreco <stelios.kakoliris@yahoo.com>
2024-05-27 22:51:01 +0100

    changed export to esm syntax

--- ftp-client/src/axios.js
+++ ftp-client/src/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-exports.api = axios.create({
+export const api = axios.create({
     baseURL: process.env.NODE_ENV === "production" ? "https://ftp-l4ir.onrender.com" : "http://127.0.0.1:8080"
 });
 
-exports.auth = axios.create({
+export const auth = axios.create({
     baseURL: process.env.NODE_ENV === "production" ? "https://auth-server-602v.onrender.com" : "http://127.0.0.1:9091"
 });
